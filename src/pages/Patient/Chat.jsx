d9a376bcd6624cc292c9e50a4837141fdf7c52cc
ladibanks1 <oladipupobankole24@gmail.com>
2024-11-29 12:36:46 +0100

    Adjust image width in Chat components for doctor and patient to improve layout consistency

--- src/pages/Patient/Chat.jsx
+++ src/pages/Patient/Chat.jsx
@@ -78,7 +78,7 @@ const Chat = () => {
             <img
               src={chosenDoctor.picture}
               alt="doctor"
-              className="w-10 inline rounded-full object-cover"
+              className="w-7 inline rounded-full object-cover"
               onClick={() => handleUser(_id)}
             />
             <p className="text-sm inline">{`${chosenDoctor.first_name} ${chosenDoctor.last_name}`}</p>
--- src/pages/Staff/Chat.jsx
+++ src/pages/Staff/Chat.jsx
@@ -70,7 +70,7 @@ const Chat = () => {
               <img
                 src={picture}
                 alt="patient"
-                className="rounded-full object-cover"
+                className="w-7 rounded-full object-cover"
                 onClick={() => handleUser(_id)}
               />
               <p className="text-center text-sm">{`${first_name} ${last_name}`}</p>
